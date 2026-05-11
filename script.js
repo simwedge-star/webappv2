dcb53a38ceae5959d3281cb2cf60b011e1a70f40
simwedge <simwedge@gmail.com>
2026-05-12 07:00:27 +0900

    debug script lad

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert("새 script.js가 실행됐어요");
+
 const loadBtn = document.getElementById("loadBtn");
 const result = document.getElementById("result");
 const dateSelect = document.getElementById("dateSelect");
