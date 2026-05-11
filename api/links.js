9b34ae10751f3224d18748d6478ec5cd046ba9bf
simwedge <simwedge@gmail.com>
2026-05-12 08:28:24 +0900

    add links api

---
+++ api/links.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  try {
+    const GAS_URL = "https://script.google.com/macros/s/AKfycbwIvGwTjWEYXPtHrdwEjU4tYoOY_YKK2O-0WYUj1Kc9C4oW0AuEZI20tKEWS_ouz-g/exec";
+    const url = `${GAS_URL}?mode=links`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
