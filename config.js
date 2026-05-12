f49deeb79250e5620b1ca91269f73897470b2c88
simwedge <simwedge@gmail.com>
2026-05-12 17:14:58 +0900

    update

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const env = typeof process !== "undefined" && process.env ? process.env : {};
 
 export const GAS_URL = env.GAS_URL || "";
 
-export const MAIN_SHEET_URL = "";
-export const DAILY_SHEET_URL = "";
+export const MAIN_SHEET_URL = "https://docs.google.com/spreadsheets/d/1FR7Vrs_TEkmD8l34aHTNaLGB_ZiTnk6nCfSbjLacGA0/edit?gid=378762600#gid=378762600";
+export const DAILY_SHEET_URL = "https://docs.google.com/spreadsheets/d/1YhbjTuymC1-txIGLgw-5SC2MOhlD3dgwLbGYjRFP-YA/edit?gid=274681049#gid=274681049";
 
 export function requireGasUrl() {
   if (!GAS_URL) {
